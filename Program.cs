f48403551eb270603acaf5a221408b838b17ad2e
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-08-23 18:25:59 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿try
+{
+  System.Console.WriteLine("Enter account data");
+  System.Console.Write("Number: ");
+  var number = int.Parse(Console.ReadLine());
+
+  System.Console.Write("Holder: ");
+  var holder = Console.ReadLine();
+
+  System.Console.Write("Initial Balance: ");
+  var initialBalance = double.Parse(Console.ReadLine());
+
+  System.Console.Write("Withdraw limit: ");
+  var withdrawLimit = double.Parse(Console.ReadLine());
+
+  var account = new Account(number, holder, initialBalance, withdrawLimit);
+
+  System.Console.Write("Enter amount for withdraw: ");
+  var withdrawAmount = double.Parse(Console.ReadLine());
+
+  account.Withdraw(withdrawAmount);
+
+  System.Console.Write($"New balance: {account.Balance:F2}");
+}
+catch (DomainException e)
+{
+  System.Console.WriteLine(e.Message);
+}
